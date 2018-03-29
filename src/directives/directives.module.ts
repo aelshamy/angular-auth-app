f5c66fe9a1233b2050050fbe5b48fe618530a8d2
Amr Mohamed <ammohamed@icct.com>
2018-03-29 18:14:54 -0400

    delete all files

--- src/directives/directives.module.ts
+++
@@ -1,9 +0,0 @@
-import { NgModule } from '@angular/core';
-import { HideIfUnauthorizedDirective } from './hide-if-unauthorized/hide-if-unauthorized.directive';
-import { DisableIfUnauthorizedDirective } from './disable-if-unauthorized/disable-if-unauthorized.directive';
-
-NgModule({
-  declarations: [HideIfUnauthorizedDirective, DisableIfUnauthorizedDirective],
-
-  exports: [HideIfUnauthorizedDirective, DisableIfUnauthorizedDirective]
-});
